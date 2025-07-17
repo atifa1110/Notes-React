c249eb313202e1a703aa1e16906acc553afb8c88
atifa1110 <atifafiorenza24@gmail.com>
2025-07-18 00:11:56 +0700

    add

--- src/components/InitAuth.jsx
+++ src/components/InitAuth.jsx
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 
-import { Icons } from '@/components/icons';
 import { useInitAuth } from '@/hooks/use-init-auth';
 
 export function InitAuth({ children }) {
@@ -10,7 +9,6 @@ export function InitAuth({ children }) {
     return (
       <div className="grid h-screen place-items-center bg-primary-foreground">
         <div className="flex items-center gap-2 text-muted-foreground">
-          <Icons.Brand />
           <h1>Loading your note...</h1>
         </div>
       </div>
